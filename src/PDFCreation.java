0fb79b2fe3161d9bcadf84585741fe1b6f2e6313
jjesusmonroy <ppchuyjjms@gmail.com>
2018-03-07 21:44:34 -0700

    succesfully loaded a pdf file

--- src/PDFCreation.java
+++ src/PDFCreation.java
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.io.File;
 import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
 
 /**
  *
@@ -13,13 +15,39 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 public class PDFCreation {
     
     public static void main(String [] args) throws IOException{
-        PDDocument document = new PDDocument();
+        /*PDDocument document = new PDDocument();
+        
+        for (int i=0;i<10; i++){
+            PDPage blankPage = new PDPage();
+            
+            document.addPage(blankPage);
+        }
         
         document.save("/home/jjesusmonroy/Documents/example.pdf");
         
         System.out.println("PDF created");
         
-        document.close();
+        document.close();*/
+        
+        loadingFile("/home/jjesusmonroy/Documents/example.pdf");
+    }
+    
+    private static void loadingFile(String ruta){
+        File file = new File(ruta);
+        PDDocument document;
+        try{
+            document = PDDocument.load(file);
+            System.out.println("PDF Loaded");
+            
+            document.addPage(new PDPage());
+            
+            document.save(ruta);
+            
+            document.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        
     }
     
 }
